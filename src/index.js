0ab7787a6ffe75d6023dfc79cf169f14fb049609
Samiul Karim <samiulkarimprodhan@gmail.com>
2021-06-04 12:57:57 +0600

    initialing seconds

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-import react from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+let date = new Date();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.timeUpdate = this.timeUpdate.bind(this);
+
+    this.state = {
+      time: undefined,
+    };
+  }
+  timeUpdate() {
+    console.log("called");
+    this.setState({ time: date.getSeconds() });
+  }
+  componentDidMount() {
+    this.interval = setInterval(this.timeUpdate, 1000);
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.state.time}</p>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
