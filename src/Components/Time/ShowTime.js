3d18c436402742104bc09faa7ee66abf8f5d99eb
SamiulKarim <samiulkarimprodhan@gmail.com>
2022-09-13 22:47:17 +0600

    Added circle progress bar

--- src/Components/Time/ShowTime.js
+++ src/Components/Time/ShowTime.js
@@ -1,4 +1,34 @@
+import "./ShowTime.css";
 const ShowTime = () => {
-  return <h3>Time</h3>;
+  return (
+    <div>
+      <svg className="progress" width="300" height="300">
+        <circle
+          className="progress__circle"
+          cx="150"
+          cy="150"
+          r="140"
+          fill="transparent"
+          stroke="green"
+          strokeWidth="20"
+        ></circle>
+
+        <text
+          className="loading"
+          fill="blue"
+          x="150"
+          y="150"
+          textAnchor="middle"
+          alignmentBaseline="middle"
+        >
+          Time
+        </text>
+      </svg>
+    </div>
+  );
 };
+const circumference = document
+  .querySelector(".progress__circle")
+  .getTotalLength();
+console.log(circumference);
 export default ShowTime;
