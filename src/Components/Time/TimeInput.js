272538bd46ec204eabdaca92e085f56ca6e4a325
SamiulKarim <samiulkarimprodhan@gmail.com>
2022-09-14 01:49:28 +0600

    Get the value from TimeInput

--- src/Components/Time/TimeInput.js
+++ src/Components/Time/TimeInput.js
@@ -1,14 +1,40 @@
+import { useRef } from "react";
 import Input from "../UI/Input";
 import "./Time.css";
 
-const Time = () => {
+const TimeInput = () => {
+  const givenHours = useRef();
+  const givenMinitue = useRef();
+  const givenSeconds = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredHours = givenHours.current.value;
+    const enteredMinitue = givenMinitue.current.value;
+    const enteredSeconds = givenSeconds.current.value;
+  };
   return (
     <div className="ui grid middle aligned time">
-      <form className="ui form">
+      <form className="ui form" onSubmit={submitHandler}>
         <div className="fields">
-          <Input label="Hours" type="number" placeholder="hours"></Input>
-          <Input label="Minitues" type="number" placeholder="minitue"></Input>
-          <Input label="Seconds" type="number" placeholder="second"></Input>
+          <Input
+            ref={givenHours}
+            label="Hours"
+            type="number"
+            placeholder="hours"
+          ></Input>
+          <Input
+            ref={givenMinitue}
+            label="Minitues"
+            type="number"
+            placeholder="minitue"
+          ></Input>
+          <Input
+            ref={givenSeconds}
+            label="Seconds"
+            type="number"
+            placeholder="second"
+          ></Input>
         </div>
         <button className="ui green button">Start</button>
         <button className="ui red button">Reset</button>
@@ -16,4 +42,4 @@ const Time = () => {
     </div>
   );
 };
-export default Time;
+export default TimeInput;
