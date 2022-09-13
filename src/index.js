a67ab4ce0c0c3334c864794988fde50a7b329325
Samiul Karim <samiulkarimprodhan@gmail.com>
2022-09-13 08:14:29 +0600

    Delete src directory

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./time"
-ReactDOM.render(<App />, document.querySelector("#root"));
--- src/time2.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const AnotherTime = (props)=>{
-    return <div><h1>{props.time}</h1></div>
-}
-export default AnotherTime;
