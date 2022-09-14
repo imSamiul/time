40302d51b6030d4f28033fdbe6898eef27d9b06f
Samiul <samiulkarimprodhan@gmail.com>
2022-09-14 19:17:02 +0600

    get the time left from given time and current time

--- src/Components/Time/ShowTime.js
+++ src/Components/Time/ShowTime.js
@@ -3,9 +3,15 @@ import CircularProgressBar from "../UI/ProgressBar";
 import "./ShowTime.css";
 const ShowTime = (props) => {
   const [percentage, setPercentage] = useState(0);
-
+  const hoursInMs = props.givenTime.hours * 3600000;
+  const minitueInMs = props.givenTime.minitue * 60000;
+  const secondsInMs = props.givenTime.seconds * 1000;
+  const nowInMs = new Date().getTime();
+  const givenTimeInMs = hoursInMs + minitueInMs + secondsInMs;
+  const dateTimeAfterGivenTime = nowInMs + givenTimeInMs;
+  const timeLeftInMs = dateTimeAfterGivenTime - nowInMs;
   return (
-    console.log(props.givenTime),
+    console.log(timeLeftInMs),
     (
       <div>
         <CircularProgressBar strokeWidth="25" sqSize="300" percentage={80} />
